744477e30c72980e1777d98d450fcbe3d203d013
Bandana Chaulagain <138704329+bandanahub@users.noreply.github.com>
2023-07-26 16:05:30 -0400

    Update cv_calculator.js

--- cv_calculator.js
+++ cv_calculator.js
@@ -1,14 +1,32 @@
-$(document) . ready (function () {
-    $('convert').click(function(){
-        var btc = $('#btc').val();
+const form = document.getElementById('converter-form');
+const amountInput = document.getElementById('amount');
+const currencySelect = document.getElementById('currency');
+const resultDiv = document.getElementById('result');
 
-        $ajax({
-            url : 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json',
-            success: function(data){
-                var rate = JSON.parse(data).bpi.USD.rate_float;
-                var usd = btc * rate;
-                $('result').text(btc + 'bitcoin equals' + usd + 'USD');
-            }
-        });
-    });
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const amount = parseFloat(amountInput.value);
+  const currency = currencySelect.value;
+
+  if (isNaN(amount)) {
+    alert('Please enter a valid amount');
+    return;
+  }
+
+  const apiKey = 'C783DF3A-7B5E-40C5-8CFE-72A5F3D34165'; 
+  const url = `https://rest.coinapi.io/v1/exchangerate/BTC/${currency}?apikey=${apiKey}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (response.ok) {
+      const convertedAmount = amount * data.rate;
+      resultDiv.textContent = `${amount} BTC is equal to ${convertedAmount.toFixed(2)} ${currency}`;
+    } else {
+      alert(`Error: ${data.error}`);
+    }
+  } catch (error) {
+    alert('Failed to fetch conversion rate');
+  }
 });
